b03e7f8a6dd2211615921cbc06c2c27a7a6ad23b
Aogon <aoi_sakai@icloud.com>
2020-05-14 20:14:15 +0900

    add layout

--- app/src/main/java/app/sakai/tororoimo/yosouyosowanai/MainActivity.kt
+++ app/src/main/java/app/sakai/tororoimo/yosouyosowanai/MainActivity.kt
@@ -2,11 +2,19 @@ package app.sakai.tororoimo.yosouyosowanai
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val people = arrayOf<String>("おっちー", "まっすー", "しんぺー", "おんじ")
+    val food = arrayOf<String>("かれー", "みそしる", "ちゃーはん", "えびちり")
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+
 }
